aab6a1fbd922ccde11deed9cb3b194686e280051
Nordine Bittich <nordine@bittich.be>
2021-06-24 14:26:21 +0200

    check url service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -131,6 +131,27 @@ export default [
     },
     callback: {
       method: 'POST',
+      url: 'http://harvest-check-url/delta'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://redpencil.data.gift/id/concept/JobStatus/scheduled'
+      }
+    },
+    callback: {
+      method: 'POST',
       url: 'http://harvesting-sameas/delta'
     },
     options: {
