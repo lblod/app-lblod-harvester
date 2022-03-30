2e4b6096caf9d39050a7e6291fb78cdf3210c451
felix <ruiz.felixruiz@gmail.com>
2022-03-30 10:28:05 +0200

    remove delta-rule

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -292,24 +292,6 @@ export default [
   },
   {
     match: {
-      graph: {
-        type: 'uri',
-        value: 'http://redpencil.data.gift/id/deltas/producer/lblod-harvester-besluiten-producer',
-      },
-    },
-    callback: {
-      url: 'http://delta-producer-json-diff-file-publisher-besluiten/delta',
-      method: 'POST',
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
-    },
-  },
-  {
-    match: {
       predicate: {
         type: 'uri',
         value: 'http://www.w3.org/ns/adms#status',
