b989df0794337c08b899af74204ac815dc2ff0f8
Claire Lovisa <lovisa.claire@gmail.com>
2022-11-09 18:30:48 +0100

    Add error-alert service to receive emails when things go wrong

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -352,4 +352,25 @@ export default [
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
   },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value:'http://open-services.net/ns/core#Error'
+      }
+    },
+    callback: {
+      url: 'http://error-alert/delta',
+      method:'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
 ];
