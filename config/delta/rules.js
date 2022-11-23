59c5690c99133082bf4debb2448e732eb8e85f32
Nordine Bittich <nordine@bittich.be>
2022-11-23 09:45:38 +0100

    improve performance by using a diff step

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -31,7 +31,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
+      gracePeriod: 150,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
@@ -75,7 +75,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
+      gracePeriod: 150,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
@@ -97,7 +97,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
+      gracePeriod: 150,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
@@ -138,6 +138,28 @@ export default [
     callback: {
       method: 'POST',
       url: 'http://harvesting-validator/delta',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
+    },
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status',
+      },
+      object: {
+        type: 'uri',
+        value: 'http://redpencil.data.gift/id/concept/JobStatus/scheduled',
+      },
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://harvesting-diff/delta',
     },
     options: {
       resourceFormat: 'v0.0.1',
@@ -208,67 +230,7 @@ export default [
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
   },
-  {
-    match: {
-      predicate: {
-        type: 'uri',
-        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-      },
-      object: {
-        type: 'uri',
-        value: 'http://redpencil.data.gift/vocabularies/deltas/Error',
-      },
-    },
-    callback: {
-      url: 'http://delta-producer-report-generator/delta',
-      method: 'POST',
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: 'uri',
-        value: 'http://www.w3.org/ns/adms#status',
-      },
-    },
-    callback: {
-      url: 'http://delta-producer-report-generator/delta',
-      method: 'POST',
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
-    },
-  },
-  {
-    match: {
-      graph: {
-        type: 'uri',
-        value: 'http://mu.semte.ch/graphs/public',
-      },
-    },
-    callback: {
-      url: 'http://delta-producer-publication-graph-maintainer-besluiten/delta',
-      method: 'POST',
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: [
-        'http://redpencil.data.gift/id/concept/muScope/deltas/initialSync',
-        'http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance',
-      ],
-    },
-  },
+
   {
     match: {
       graph: {
