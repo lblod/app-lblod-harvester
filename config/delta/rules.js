d5b2ff4c42a50563cec3ddc686d7a25f774d833d
Nordine Bittich <nordine@bittich.be>
2022-12-02 12:55:36 +0100

    rollback decrease grace period to reduce risks of steps being duplicated

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -31,7 +31,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 500,
+      gracePeriod: 1000,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
@@ -75,7 +75,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 500,
+      gracePeriod: 1000,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
@@ -97,7 +97,7 @@ export default [
     },
     options: {
       resourceFormat: 'v0.0.1',
-      gracePeriod: 500,
+      gracePeriod: 1000,
       ignoreFromSelf: true,
       optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
     },
