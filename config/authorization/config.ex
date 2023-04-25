bd2ae4fa93a227d211379459b6e364e65af30de0
benjay10 <ben.behaeghe@redpencil.io>
2023-04-25 14:54:51 +0200

    Restore changes to the Login feature
    This branch got accidentally merged into another branch where deletes
    were added in the pipeline. That merge has been reverted and that branch
    has been merged into master. Later merges brought changes from master
    into this branch, including the revert, wich made all the changes for
    this branch undone. Using `git diff` we were able to retrieve the
    changes from an earlier commit.

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -6,6 +6,16 @@ alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 alias Acl.Accessibility.ByQuery, as: AccessByQuery
 
 defmodule Acl.UserGroups.Config do
+  defp logged_in_user() do
+    %AccessByQuery{
+      vars: [],
+      query: "PREFIX session: <http://mu.semte.ch/vocabularies/session/>
+              SELECT DISTINCT ?account WHERE {
+                  <SESSION_ID> session:account ?account.
+        }"
+      }
+  end
+
   defp can_access_worship_deltas() do
     %AccessByQuery{
       vars: [ ],
@@ -34,7 +44,7 @@ defmodule Acl.UserGroups.Config do
       %GroupSpec{
         name: "harvesting",
         useage: [:write, :read_for_write, :read],
-        access: %AlwaysAccessible{},
+        access: logged_in_user(),
         graphs: [ %GraphSpec{
                     graph: "http://mu.semte.ch/graphs/harvesting",
                     constraint: %ResourceConstraint{
