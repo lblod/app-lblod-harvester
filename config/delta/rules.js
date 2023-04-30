e0324bd8552116946f61eb5556db68ea86102e18
benjay10 <ben.behaeghe@redpencil.io>
2023-04-30 05:26:23 +0200

    Add singleton-job in deltanotifier config

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -22,6 +22,28 @@ export default [
       },
       object: {
         type: 'uri',
+        value: 'http://redpencil.data.gift/id/concept/JobStatus/scheduled',
+      },
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://harvesting-singleton-jobs/delta',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: ['http://redpencil.data.gift/id/concept/muScope/deltas/initialSync'],
+    },
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status',
+      },
+      object: {
+        type: 'uri',
         value: 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached',
       },
     },
