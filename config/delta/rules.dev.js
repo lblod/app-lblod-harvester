e9e8f79c8b99dccfac574ad3a9da8d7929e21acb
Nordine Bittich <nordine@bittich.be>
2023-12-13 15:59:45 +0100

    disable compression

--- config/delta/rules.dev.js
+++ config/delta/rules.dev.js
@@ -34,28 +34,28 @@ export default [
       ignoreFromSelf: true,
     },
   },
-
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/ns/adms#status",
-      },
-      object: {
-        type: "uri",
-        value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
-      },
-    },
-    callback: {
-      method: "POST",
-      url: "http://harvest_compression/delta",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-    },
-  },
+  //
+  // {
+  //   match: {
+  //     predicate: {
+  //       type: "uri",
+  //       value: "http://www.w3.org/ns/adms#status",
+  //     },
+  //     object: {
+  //       type: "uri",
+  //       value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
+  //     },
+  //   },
+  //   callback: {
+  //     method: "POST",
+  //     url: "http://harvest_compression/delta",
+  //   },
+  //   options: {
+  //     resourceFormat: "v0.0.1",
+  //     gracePeriod: 1000,
+  //     ignoreFromSelf: true,
+  //   },
+  // },
   {
     match: {
       predicate: {
