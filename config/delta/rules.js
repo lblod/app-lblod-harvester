aa55b4e17fce4b71362504fc0f42673c03ba0d8d
Nordine Bittich <nordine@bittich.be>
2024-01-26 11:16:35 +0100

    cleanup

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -76,6 +76,27 @@ export default [
       ignoreFromSelf: true,
     },
   },
+  {
+    match: {
+      predicate: {
+        type: "uri",
+        value: "http://www.w3.org/ns/adms#status",
+      },
+      object: {
+        type: "uri",
+        value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
+      },
+    },
+    callback: {
+      method: "POST",
+      url: "http://harvest_cleanup/delta",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+    },
+  },
   // {
   //   match: {
   //     predicate: {
