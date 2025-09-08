d33b76706f1d400ab7279a9b1ab34fea76b85b4c
Niels Vandekeybus <progster@gmail.com>
2025-09-08 08:47:58 +0200

    remove delta rule for experimental harvest_load service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -201,28 +201,6 @@ export default [
     },
     callback: {
       method: "POST",
-      url: "http://harvest_load/delta",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      sendMatchesOnly: true,
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/ns/adms#status",
-      },
-      object: {
-        type: "uri",
-        value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
-      },
-    },
-    callback: {
-      method: "POST",
       url: "http://harvest_sameas/delta",
     },
     options: {
