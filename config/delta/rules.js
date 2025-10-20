2a0f357d58b7b7efa13db96fd4a9038922dcc599
Niels Vandekeybus <progster@gmail.com>
2025-10-20 13:27:15 +0200

    disable delta generation for local sources

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -157,28 +157,6 @@ export default [
     },
     callback: {
       method: "POST",
-      url: "http://harvest_gen_delta/delta",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      sendMatchesOnly: true,
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/ns/adms#status",
-      },
-      object: {
-        type: "uri",
-        value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
-      },
-    },
-    callback: {
-      method: "POST",
       url: "http://harvest_diff/delta",
     },
     options: {
